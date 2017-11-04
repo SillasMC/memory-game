910d494187add3a4525f807a261e688268311c5a
Sillas Camara <sillasmc@gmail.com>
2017-11-03 23:53:05 -0300

    Power and Strict actions

--- js/main.js
+++ js/main.js
@@ -1,3 +1,62 @@
 $(document).ready(function() {
+	// Global variables
+	var $powerButton	= $("#power-button-id");
+	var isGameTurnedOn	= getPowerButtonStatus();
+	var $strictButton	= $("#strict-button-id");
+	var isStrict		= getStrictButtonStatus();
 
+	// Get Status of the power button
+	function getPowerButtonStatus () {
+		return $powerButton.hasClass("power-switch-active");
+	}
+
+	function turnOffGame () {
+		$powerButton.removeClass("power-switch-active");
+		$powerButton.addClass("power-switch");
+
+		//Turn off other elements
+		turnOffStrict();
+	}
+
+	// Power button action
+	$powerButton.on("click", function () {
+		if (isGameTurnedOn) {
+			turnOffGame();
+		}
+		else {
+			$powerButton.removeClass("power-switch");
+			$powerButton.addClass("power-switch-active");
+		}
+
+		isGameTurnedOn = !isGameTurnedOn;
+	});
+
+	// Get Status of the strict button
+	function getStrictButtonStatus () {
+		return $strictButton.hasClass("strict-div-active");
+	}
+
+	function turnOnStrict () {
+		$strictButton.removeClass("strict-div");
+		$strictButton.addClass("strict-div-active");
+	}
+
+	function turnOffStrict () {
+		$strictButton.removeClass("strict-div-active");
+		$strictButton.addClass("strict-div");
+	}
+
+	// Strict button action
+	$strictButton.on("click", function () {
+		if (isGameTurnedOn) {
+			if (isStrict) {
+				turnOffStrict();
+			}
+			else {
+				turnOnStrict();
+			}
+
+			isStrict = !isStrict;
+		}
+	});
 });
